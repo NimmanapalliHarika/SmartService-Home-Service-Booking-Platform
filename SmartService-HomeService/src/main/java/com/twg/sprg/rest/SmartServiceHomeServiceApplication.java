dd79259f7198c234be53005b6d8a1e685b0cd9b4
NimmanapalliHarika <nimmanapalliharika@gmail.com>
2025-06-07 16:27:13 +0530

    first commit

---
+++ SmartService-HomeService/src/main/java/com/twg/sprg/rest/SmartServiceHomeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.twg.sprg.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartServiceHomeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartServiceHomeServiceApplication.class, args);
+	}
+
+}
